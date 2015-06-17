67c87b945ad88f34abe6625a6040cfa030304aeb
Arkady Pevzner <arkady@famo.us>
2015-06-17 11:06:10 -0700

    Remove child after 1 second

--- src/index.js
+++ src/index.js
@@ -11,13 +11,17 @@ var root = FamousEngine.createScene().addChild();
 root.setSizeMode(1, 1, 1);
 root.setAbsoluteSize(400, 400);
 
+var children = [];
 var child;
 var domEl;
 for (var i = 0; i < 20; i++) {
     child = root.addChild();
-    child.setPosition(Math.random() * 800, Math.random() * 800);
+    if (i > 0) {
+        child.setPosition(Math.random() * 800, Math.random() * 800);
+    }
     child.setSizeMode(1, 1, 1);
     child.setAbsoluteSize(50, 50);
+    children.push(child);
     domEl = new DOMElement(child);
     domEl.setProperty('background-color', 'black');
     domEl.setProperty('color', 'white');
@@ -28,4 +32,8 @@ setTimeout(function(){
     domEl = new DOMElement(root);
     domEl.setProperty('border', '1px solid black');
     domEl.setProperty('overflow', 'scroll');
+}, 500);
+
+setTimeout(function(){
+    root.removeChild(children[0]);
 }, 1000);
