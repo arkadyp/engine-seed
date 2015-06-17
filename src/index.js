b79d1715c9976f16be50450696307efde8117104
Arkady Pevzner <arkady@famo.us>
2015-06-17 10:08:12 -0700

    async-children-append example

--- src/index.js
+++ src/index.js
@@ -7,32 +7,25 @@ var FamousEngine = require('famous/core/FamousEngine');
 // Boilerplate code to make your life easier
 FamousEngine.init();
 
-// Initialize with a scene; then, add a 'node' to the scene root
-var logo = FamousEngine.createScene().addChild();
+var root = FamousEngine.createScene().addChild();
+root.setSizeMode(1, 1, 1);
+root.setAbsoluteSize(400, 400);
 
-// Create an [image] DOM element providing the logo 'node' with the 'src' path
-new DOMElement(logo, { tagName: 'img' })
-    .setAttribute('src', './images/famous_logo.png');
+var child;
+var domEl;
+for (var i = 0; i < 20; i++) {
+    child = root.addChild();
+    child.setPosition(Math.random() * 800, Math.random() * 800);
+    child.setSizeMode(1, 1, 1);
+    child.setAbsoluteSize(50, 50);
+    domEl = new DOMElement(child);
+    domEl.setProperty('background-color', 'black');
+    domEl.setProperty('color', 'white');
+    domEl.setContent(i);
+}
 
-// Chainable API
-logo
-    // Set size mode to 'absolute' to use absolute pixel values: (width 250px, height 250px)
-    .setSizeMode('absolute', 'absolute', 'absolute')
-    .setAbsoluteSize(250, 250)
-    // Center the 'node' to the parent (the screen, in this instance)
-    .setAlign(0.5, 0.5)
-    // Set the translational origin to the center of the 'node'
-    .setMountPoint(0.5, 0.5)
-    // Set the rotational origin to the center of the 'node'
-    .setOrigin(0.5, 0.5);
-
-// Add a spinner component to the logo 'node' that is called, every frame
-var spinner = logo.addComponent({
-    onUpdate: function(time) {
-        logo.setRotation(0, time / 1000, 0);
-        logo.requestUpdateOnNextTick(spinner);
-    }
-});
-
-// Let the magic begin...
-logo.requestUpdate(spinner);
+setTimeout(function(){
+    domEl = new DOMElement(root);
+    domEl.setProperty('border', '1px solid black');
+    domEl.setProperty('overflow', 'scroll');
+}, 1000);
